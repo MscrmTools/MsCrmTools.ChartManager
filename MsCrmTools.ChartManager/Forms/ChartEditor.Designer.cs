256247f60d6636bcef6d22adb0307ebfe6f058ab
Tanguy TOUZARD <tanguy92@hotmail.com>
2017-03-21 18:13:29 +0100

    Initial commit

---
+++ MsCrmTools.ChartManager/Forms/ChartEditor.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace MsCrmTools.ChartManager.Forms
+{
+    partial class ChartEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTitle = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lblDataDescription = new System.Windows.Forms.Label();
+            this.lblVisualizationDescription = new System.Windows.Forms.Label();
+            this.tecDataDescription = new ICSharpCode.TextEditor.TextEditorControl();
+            this.tecVisualizationDescription = new ICSharpCode.TextEditor.TextEditorControl();
+            this.pnlTitle.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTitle
+            // 
+            this.pnlTitle.BackColor = System.Drawing.Color.White;
+            this.pnlTitle.Controls.Add(this.label1);
+            this.pnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitle.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pnlTitle.Name = "pnlTitle";
+            this.pnlTitle.Size = new System.Drawing.Size(705, 65);
+            this.pnlTitle.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chart Editor";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnUpdate);
+            this.panel2.Controls.Add(this.btnClose);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 514);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(705, 33);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.Location = new System.Drawing.Point(547, 3);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(628, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.Location = new System.Drawing.Point(102, 2);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(601, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDescription.Location = new System.Drawing.Point(102, 23);
+            this.txtDescription.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(601, 20);
+            this.txtDescription.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(0, 4);
+            this.lblName.Margin = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Name";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(0, 25);
+            this.lblDesc.Margin = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(60, 13);
+            this.lblDesc.TabIndex = 5;
+            this.lblDesc.Text = "Description";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.lblDesc);
+            this.panel1.Controls.Add(this.txtDescription);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(705, 45);
+            this.panel1.TabIndex = 7;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 110);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tecDataDescription);
+            this.splitContainer1.Panel1.Controls.Add(this.lblDataDescription);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tecVisualizationDescription);
+            this.splitContainer1.Panel2.Controls.Add(this.lblVisualizationDescription);
+            this.splitContainer1.Size = new System.Drawing.Size(705, 404);
+            this.splitContainer1.SplitterDistance = 201;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // lblDataDescription
+            // 
+            this.lblDataDescription.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblDataDescription.Location = new System.Drawing.Point(0, 0);
+            this.lblDataDescription.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDataDescription.Name = "lblDataDescription";
+            this.lblDataDescription.Size = new System.Drawing.Size(104, 201);
+            this.lblDataDescription.TabIndex = 6;
+            this.lblDataDescription.Text = "Data Description";
+            // 
+            // lblVisualizationDescription
+            // 
+            this.lblVisualizationDescription.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblVisualizationDescription.Location = new System.Drawing.Point(0, 0);
+            this.lblVisualizationDescription.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblVisualizationDescription.Name = "lblVisualizationDescription";
+            this.lblVisualizationDescription.Size = new System.Drawing.Size(104, 200);
+            this.lblVisualizationDescription.TabIndex = 8;
+            this.lblVisualizationDescription.Text = "Presentation Desc.";
+            // 
+            // tecDataDescription
+            // 
+            this.tecDataDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tecDataDescription.IsReadOnly = false;
+            this.tecDataDescription.Location = new System.Drawing.Point(104, 0);
+            this.tecDataDescription.Margin = new System.Windows.Forms.Padding(2);
+            this.tecDataDescription.Name = "tecDataDescription";
+            this.tecDataDescription.Size = new System.Drawing.Size(601, 201);
+            this.tecDataDescription.TabIndex = 7;
+            // 
+            // tecVisualizationDescription
+            // 
+            this.tecVisualizationDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tecVisualizationDescription.IsReadOnly = false;
+            this.tecVisualizationDescription.Location = new System.Drawing.Point(104, 0);
+            this.tecVisualizationDescription.Margin = new System.Windows.Forms.Padding(2);
+            this.tecVisualizationDescription.Name = "tecVisualizationDescription";
+            this.tecVisualizationDescription.Size = new System.Drawing.Size(601, 200);
+            this.tecVisualizationDescription.TabIndex = 9;
+            // 
+            // ChartEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 547);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlTitle);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "ChartEditor";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Chart Editor";
+            this.Load += new System.EventHandler(this.ChartEditor_Load);
+            this.pnlTitle.ResumeLayout(false);
+            this.pnlTitle.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTitle;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label lblDataDescription;
+        private System.Windows.Forms.Label lblVisualizationDescription;
+        private ICSharpCode.TextEditor.TextEditorControl tecDataDescription;
+        private ICSharpCode.TextEditor.TextEditorControl tecVisualizationDescription;
+    }
+}
